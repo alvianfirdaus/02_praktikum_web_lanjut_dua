d407954ec716f4b4bf77e969db6a120aa257cbdc
= <alvianfirdaus896@gmail.com>
2023-02-22 10:34:45 +0700

    update

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        echo '2141720022 Alvian Nur Firdaus | About Controller';
+    }
+}
--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,15 +6,7 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index(){
-        echo 'Seloamat Datang';
-    }
-    public function about(){
-        echo '2141720022 Alvian Nur Firdaus';
-    }
-    public function articles($id){
-        echo 'Halaman Artikel dengan Id ' .$id;
-    }
+    //
 }
 
 
